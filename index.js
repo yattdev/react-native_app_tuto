b422b70219b022981558202b99c920e354789f80
yattdev <yattdeveloper@gmail.com>
2021-12-28 17:15:00 +0100

    Update index.js
    - To add PushNotification config from github

--- index.js
+++ index.js
@@ -3,7 +3,17 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './src/App';
 import {name as appName} from './app.json';
+import PushNotification from 'react-native-push-notification';
+
+// Must be outside of any component LifeCycle (such as `componentDidMount`).
+PushNotification.configure({
+  // (required) Called when a remote is received or opened, or local notification is opened
+  onNotification: function (notification) {
+    console.log('NOTIFICATION:', notification);
+  },
+  requestPermissions: Platform.OS === 'ios',
+});
 
 AppRegistry.registerComponent(appName, () => App);
